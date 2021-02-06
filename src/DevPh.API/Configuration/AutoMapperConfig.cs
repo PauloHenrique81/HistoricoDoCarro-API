7aff4509ac7fa73ee1e9296d69fa5e35849e510b
Paulo Henrique <p.henrique@intcom.com.br>
2021-02-06 18:36:18 -0300

    Add project files.

---
+++ src/DevPh.API/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DevIO.Business.Models;
+using DevPh.Api.ViewModels;
+
+namespace DevPh.Api.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
